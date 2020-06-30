44467408012d0d9e5b5a0180a848281b13ae465a
Pedro <pedrodxavier@hotmail.com>
2020-06-29 22:53:29 -0300

    Add project files.

---
+++ SimpleFramework.Utils/AppConfig/AppConfigHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+
+namespace SimpleFramework.Utils.AppConfig
+{
+    public class AppConfigHelper
+    {
+        public static string GetConnectionString(string key)
+        {
+            return ConfigurationManager.ConnectionStrings[key].ConnectionString;
+        }
+
+        public static void ChangeConnectionStringDynamically(string key, string value)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            config.ConnectionStrings.ConnectionStrings[key].ConnectionString = value;
+            config.ConnectionStrings.ConnectionStrings[key].ProviderName = "System.Data.SqlClient";
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        public static void AddSetting(string key, string value)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings.Add(key, value);
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        public static void UpdateSetting(string key, string value)
+        {
+
+        }
+
+    }
+}
---
+++ SimpleFramework.Utils/Xml/XmlHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SimpleFramework.Utils.Xml
+{
+    public static class XmlHelper
+    {
+        public static string ConvertListToXml(IEnumerable<string> list)
+        {
+            return new XElement("list", list.Where(c => c != null).Select(c => new XElement("item", c))).ToString();
+        }
+
+        public static string ConvertListToXml(IEnumerable<int> list)
+        {
+            return new XElement("list", list.Select(c => new XElement("item", c))).ToString();
+        }
+    }
+}
