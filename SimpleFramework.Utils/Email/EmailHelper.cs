45907439a908eb976c6e298e4a9a54c74bb82521
Pedro <pedrodxavier@hotmail.com>
2020-08-03 10:13:13 -0300

    EmailHelper in progress.

--- SimpleFramework.Utils/Email/EmailHelper.cs
+++ SimpleFramework.Utils/Email/EmailHelper.cs
@@ -1,22 +1,79 @@
-﻿using System.Threading.Tasks;
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace SimpleFramework.Utils.Email
 {
-    public  class EmailHelper
+    public class EmailHelper
     {
        
         //todo: in progress :)
 
 
-        public bool SendMail()
+        public bool SendMail(EmailInfo emailInfo)
         {
-            return true;
+            var message = new MailMessage
+            {
+                IsBodyHtml = emailInfo.UseHtml,
+                From = new MailAddress(emailInfo.From, emailInfo.FromName),
+            };
+            
+            foreach (var repicient in emailInfo.Recipients)
+            {
+                message.To.Add(new MailAddress(repicient));
+            }
+
+            foreach (var recipient in emailInfo.RecipientsCc)
+            {
+                message.To.Add(new MailAddress(recipient));
+            }
+
+            message.Subject = emailInfo.Subject;
+            message.Body = emailInfo.Body;
+            
+            using (var client = new SmtpClient(emailInfo.Host, emailInfo.Port))
+            {
+                client.Credentials = new NetworkCredential(emailInfo.CredentialUser, emailInfo.CredentialPassword);
+                client.EnableSsl = emailInfo.UseSsl;
+                try
+                {
+                    client.Send(message);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
         }
+    
 
 
         public async Task<bool> SendMailAsync()
         {
             return await Task.FromResult<bool>(true);
+        }
+
+        // Read Email 
+
+
+        // ETC.
+
+        private void Validation(EmailInfo emailInfo)
+        {
+            var errors = new List<Exception>();
+
+            if(!new EmailAddressAttribute().IsValid(emailInfo.From))
+            {
+
+            }
+
+
+            throw new AggregateException(errors);
         }
 
     }
--- SimpleFramework.Utils/Email/EmailInfo.cs
+++ SimpleFramework.Utils/Email/EmailInfo.cs
@@ -1,17 +1,32 @@
-﻿namespace SimpleFramework.Utils.Email
+﻿using iTextSharp.text;
+using System.Collections.Generic;
+
+namespace SimpleFramework.Utils.Email
 {
     public class EmailInfo
     {
         public string From { get; set; }
 
+        public string FromName { get; set; }
+
+        public IEnumerable<string> Recipients { get; set; }
+
+        public IEnumerable<string> RecipientsCc { get; set; }
+
         public string Subject { get; set; }
 
         public string Host { get; set; }
+
+        public string Body { get; set; }
 
         public int Port { get; set; }
 
         public bool UseSsl { get; set; }
 
         public bool UseHtml { get; set; }
+
+        public List<byte[]> Attachments { get; set; }
+        public string CredentialUser { get; set; }
+        public string CredentialPassword { get; set; }
     }
 }
